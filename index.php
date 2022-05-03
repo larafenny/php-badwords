927719c954dce77d8c748583d07d902753307c62
Lara Filippone <lara.filippone@gmail.com>
2022-05-03 09:04:08 +0200

    added variable that contains a text

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.*/
 
 // creo array che contiene parolacce
-$badWordsArray=array('a', 'b', 'c');
+$badWordsArray=array('Lorem', 'dolor', 'anim');
+
+// inserisco un testo qualunque
+$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 
 
